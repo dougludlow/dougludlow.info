6d12c58aa1bd76351bb69d9ac3979f875819c636
Douglas Ludlow <dludlow@cityofboise.org>
2014-07-18 10:11:27 -0600

    Created controller, updated contact.

---
+++ dougludlow.info/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using dougludlow.info.Models;
+using Karbon.Cms.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dougludlow.info.Controllers
+{
+    public class HomeController : KarbonController
+    {
+        public override ActionResult Index()
+        {
+            return base.Index();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MessageModel model)
+        {
+            if (ViewData.ModelState.IsValid)
+            {
+                using(var smtp = new SmtpClient())
+                {
+                    var message = new MailMessage();
+                    message.To.Add("doug.ludlow@gmail.com");
+                    message.Subject = "dougludlow.info: " + model.Name;
+                    message.From = new MailAddress(model.Email);
+                    message.Body = model.Message;
+                    smtp.Send(message);
+                }
+            }
+
+            return Redirect("/");
+            //return base.Index();
+        }
+    }
+}
